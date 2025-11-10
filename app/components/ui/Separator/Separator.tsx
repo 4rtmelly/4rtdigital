a0f98f8a352352ebaadd955058a649e60ebed232
4rtmelly <ngy.amelie@gmail.com>
2025-11-10 16:27:38 +0100

    header, hero and intro page

---
+++ app/components/ui/Separator/Separator.tsx
@@ -0,0 +1,12 @@
+export default function Separator({ label = "[about]" }) {
+  return (
+    <>
+      <div className=" w-full items-center">
+        <div className="flex flex-row items-center">
+          <div className="flex w-full h-px bg-white" />
+          <span className="text-white  ">{label}</span>
+        </div>
+      </div>
+    </>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,96 @@
-import Image from "next/image";
+import Image from "next/image"
+import Link from "next/link"
+import DecryptedText from "./components/ui/DecryptedText"
+import Separator from "./components/ui/Separator/Separator"
 
 export default function Home() {
   return (
     <>
-    <div className="mx-auto flex flex-col items-center justify-center">
-
-    </div>
+      <div className="mx-auto flex flex-col items-center justify-center font-sans">
+        {/* HEADER */}
+        <div className="w-full p-10 flex flex-row justify-between ">
+          <span className="text-white font-mono"> melly4rt presents</span>
+          <div className="flex flex-col items-center justify-center">
+            <Link className="text-white" href="./artists">
+              ARTISTS.
+            </Link>
+            <Link className="text-white" href="./contact">
+              CONTACT.
+            </Link>
+          </div>
+        </div>
+        {/* HERO SECTION */}
+        <section className="relative mx-auto w-4/5 h-dvh flex flex-col items-center justify-center">
+          <div className="w-full flex flex-row justify-around">
+            {/* <span> [SHOW] </span> */}
+            <DecryptedText
+              text="[SHOW]"
+              speed={100}
+              maxIterations={20}
+              characters="ABCD1234!?"
+              className="revealed"
+              parentClassName="all-letters"
+              encryptedClassName="encrypted"
+              animateOn="view"
+              revealDirection="center"
+            />
+            <DecryptedText
+              text="[YOUR]"
+              speed={100}
+              maxIterations={20}
+              characters="ABCD1234!?"
+              className="revealed"
+              parentClassName="all-letters"
+              encryptedClassName="encrypted"
+              animateOn="view"
+              revealDirection="center"
+            />{" "}
+            <DecryptedText
+              text="[ART]"
+              speed={100}
+              maxIterations={20}
+              characters="ABCD1234!?"
+              className="revealed"
+              parentClassName="all-letters"
+              encryptedClassName="encrypted"
+              animateOn="view"
+              revealDirection="center"
+            />
+          </div>
+          <video className="opacity-45 h-dvh" muted autoPlay loop playsInline>
+            <source src="/images/heromp.mp4" type="video/mp4" />
+            Votre navigateur ne supporte pas la vidéo HTML5.
+          </video>
+          {/* hero title */}
+          <div className="absolute inset-0 flex flex-col items-center justify-center z-1">
+            <h1 className="italic text-white text-5xl font-sans text-center">
+              4rt digital{" "}
+              <span className="italic text-white text-5xl font-mono ">
+                gallery
+              </span>
+            </h1>
+          </div>
+          <p className="w-full flex justify-start">
+            DISCOVER EMERGENT ARTIST <br /> THROUGH MONTHLY CHANGES
+          </p>
+        </section>
+        {/* INTRO SECTION */}
+        <section className=" mx-auto w-4/5 min-h-screen flex flex-col items-center justify-between">
+          <div className="items-center justify-start">
+            <Separator label="ABLE TO DISPLAY YOUR ART EVERY MONTH" />
+          </div>
+          <Image
+            className="items-center justify-center"
+            src="/images/bwflower.jpg"
+            width={350}
+            height={600}
+            alt="orchid"
+          />
+          <div className="items-center justify-end">
+            <Separator label="SHOW YOUR POTENTIAL TO THE WORLD" />
+          </div>
+        </section>
+      </div>
     </>
-  );
+  )
 }
